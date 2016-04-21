5638fde15509e5c226d4b782915f5d9397c1db67
Kim Blackmore <Kim Blackmore>
2016-04-21 20:34:25 +1000

    Add dictionary file and seed data

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160417083007) do
+ActiveRecord::Schema.define(version: 20160421083007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "people", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.datetime "birthdate"
+  create_table "words", force: :cascade do |t|
+    t.string "text"
   end
 
 end
